8be6430c9df5c3c90b1d0957602f0081db81b42d
= <=>
2021-01-06 16:13:29 +0100

    Menu bar added; Password input type changed

--- app/src/main/java/com/example/login/Profil.java
+++ app/src/main/java/com/example/login/Profil.java
@@ -8,36 +8,14 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
-import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class Store extends AppCompatActivity {
-
-    public static ArrayList<Product> products;
-
-    public static String[] names = {"hello", "hello", "hello"};
+public class Profil extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_store);
-        products = new ArrayList<>();
-        products.add(new Product("Livadski med", "800", "Novkovic", "pic1"));
-        products.add(new Product("Bagremov med", "1000", "Novkovic", "pic2"));
-        products.add(new Product("Šumski med", "1000", "Novkovic", "pic3"));
-        products.add(new Product("Suncokret med", "1000", "Novkovic", "pic4"));
-        products.add(new Product("Coko med", "1200", "Mijailovic", "pic9"));
-        setTitle("Prodavnica");
-
-        ProductsAdapter adapter = new ProductsAdapter(this, products);
-        ListView listView = (ListView) findViewById(R.id.listView);
-        listView.setAdapter(adapter);
+        setContentView(R.layout.activity_profil);
+        setTitle("Profil");
     }
 
     @Override
